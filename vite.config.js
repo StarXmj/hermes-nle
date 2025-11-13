b57bdd4b704fbb68a18f05b4efe2b547ef685168
StarXmj <mateo.joly02@gmail.com>
2025-11-13 12:11:23 +0100

    Fix: Lancement de npm audit fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // --- AJOUTEZ CETTE SECTION ---
+  // C'est la correction pour l'erreur "node:url"
+  resolve: {
+    alias: {
+      'node:url': 'url'
+    }
+  }
+  // --- FIN DE L'AJOUT ---
 })
